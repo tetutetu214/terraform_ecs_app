c7783a7f5f2d910c78076e614c71aba0d9dfb8c2
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-17 17:14:16 +0900

    fix

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-    # get top_index_url
-    # assert_response :success
+  test "should get index" do
+    get root_path
+    assert_response :success
   end
 
 end
